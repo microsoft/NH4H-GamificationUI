2bc418cb160d53a4e29e785b1477eeb7c53dbe37
Phil Jirsa <phil.jirsa@microsoft.com>
2021-08-23 09:41:27 -0500

    Refactor to functional components

---
+++ src/components/SignInButton.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { useMsal } from "@azure/msal-react";
+import { loginRequest } from "../authConfig";
+import Button from "react-bootstrap/Button";
+
+function handleLogin(instance) {
+    instance.loginPopup(loginRequest).catch(e => {
+        console.error(e);
+    });
+}
+
+/**
+ * Renders a button which, when selected, will open a popup for login
+ */
+export const SignInButton = () => {
+    const { instance } = useMsal();
+
+    return (
+        <Button variant="secondary" className="ml-auto" onClick={() => handleLogin(instance)}>Sign in</Button>
+    );
+}
--- src/components/activitylist.js
+++ src/components/activitylist.js
@@ -1,78 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Accordion, Card, Icon } from 'semantic-ui-react';
 import ActivityListItem from './activityListItem';
 
-class ActivityList extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      myActivityGroups: [],
-    //   challenges:[],
-      activeIndex: 0
-    }
-  }
+function ActivityList({ myActivityGroups, email }) {
+  const [activeIndex, setActiveIndex] = useState(-1);
 
-  handleClick = (e, titleProps) => {
+  function handleClick(e, titleProps) {
     const { index } = titleProps
-    const { activeIndex } = this.state
     const newIndex = activeIndex === index ? -1 : index
 
-    this.setState({ activeIndex: newIndex })
+    setActiveIndex(newIndex);
   }
 
-  // componentDidUpdate(prevProps,prevState) {
-  //   if(prevProps.teams != this.props.teams){
-  //     let newt=this.groupBy(this.props.teams,'activityGroupId');
-  //     let newc= Object.getOwnPropertyNames(newt);
-  //     this.setState({
-  //       teams:newt,
-  //       challenges:newc
-  //     });
-  //   }
-  // }
-
-  getAllActivityGroups=()=>{
-    if(this.props.myActivityGroups){
-    return this.props.myActivityGroups.map((ag, index)=>(
-      <>
-
-      <div key={index} >
-      <Accordion.Title
-        active={this.state.activeIndex === index}
-        index={index}
-        onClick={this.handleClick}
-        >
-        <Icon name='dropdown' />
-       {ag.activityGroupName}
-      </Accordion.Title>
-      <Accordion.Content active={this.state.activeIndex === index}> 
-        <Card.Group>
-          {this.getActivityListItems(ag.activities)}
-        </Card.Group>
-      </Accordion.Content>
-      </div>
-      </>
+  function getAllActivityGroups() {
+    return myActivityGroups && myActivityGroups.map((ag, index) => (      
+        <div key={index} >
+          <Accordion.Title
+            active={activeIndex === index}
+            index={index}
+            onClick={handleClick}
+          >
+            <Icon name='dropdown' />
+            {ag.activityGroupName}
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === index}>
+            <Card.Group>
+              {getActivityListItems(ag.activities)}
+            </Card.Group>
+          </Accordion.Content>
+        </div>
     ));
-    }
   }
 
-  getActivityListItems=(activities)=>{
-    return activities.map( ({activityId, activityName, activityDesc, activityActionLink, activityPoints, pointsEarned, activityBadge, activityGroupId, manual}) => ( 
-      <ActivityListItem 
+  function getActivityListItems(activities) {
+    return activities.map(({ activityId, activityName, activityDesc, activityActionLink, activityPoints, pointsEarned, activityBadge, activityGroupId, manual }) => (
+      <ActivityListItem
         key={activityId} id={activityId} points={activityPoints} pointsEarned={pointsEarned}
         name={activityName} description={activityDesc} link={activityActionLink} manual={manual}
-        challengeName={activityGroupId} userEmail={this.props.email}
+        challengeName={activityGroupId} userEmail={email}
       />
     ))
-    }
-  
-  render() {
-    return(
-      <Accordion fluid styled>
-        {this.getAllActivityGroups()}      
-      </Accordion>
-    );
   }
+
+  return myActivityGroups && email && (
+    <Accordion fluid styled>
+      {getAllActivityGroups()}
+    </Accordion>
+  );
 }
 
 export default ActivityList;
